fbf7eb7cfc4c28e08580095ace53e245a6569f37
christopher j gore <christophergore.xyz@gmail.com>
2016-05-17 19:04:33 -0400

    moved some things around, renamed some bits for modular stuff

--- src/js/renderer.js
+++ src/js/renderer.js
@@ -6,8 +6,6 @@ const BOTTOM_EDGE = 1;
 const HORIZONTAL_OFFSET = LEFT_EDGE - ((RIGHT_EDGE - LEFT_EDGE) / 2);
 const MANDEL_RATIO = (RIGHT_EDGE - LEFT_EDGE) / (BOTTOM_EDGE - TOP_EDGE);
 
-      var _mandelRatio = (initRightEdge - initLeftEdge) / (initBottomEdge - initTopEdge);
-
 const DEFAULT_SETTINGS = {
     palette: 'rainbow',
     maxIterations: 1000,
@@ -19,15 +17,29 @@ const DEFAULT_SETTINGS = {
 };
 
 class Renderer {
-    constructor(canvasContext, options) {
-        this._context = canvasContext;
+    constructor(canvas, options) {
+        this._canvas = canvas;
+        this._context = this._canvas.getContext('2d');
+        this._imageData = this._context.createImageData(this._canvas.width, this._canvas.height);
+        this._data = this._imageData.data;
+
+        this._imageRatio = this._imageData.width / this._imageData.height;
+
         this._maxIterations = options.maxIterations || DEFAULT_SETTINGS.maxIterations;
         this._mandelbrotColor = options.mandelbrotColor || DEFAULT_SETTINGS.mandelbrotColor;
         this._palette = require('palette')[options.palette || DEFAULT_SETTINGS.palette];
-}
+    }
+
+    get DataUrl() {
+        return this._canvas.toDataURL('image/png');
+    }
 
     plot(x, y, color) {
-        this.context
+        var dataIndex = (y * this._imageData.width + x) * 4;
+        this._data[dataIndex] = color.r;
+        this._data[dataIndex + 1] = color.g;
+        this._data[dataIndex + 2] = color.b;
+        this._data[dataIndex + 3] = 255; //max saturation
     }
 
     render(scale, dx0, dy0) {
@@ -35,50 +47,59 @@ class Renderer {
         var dx = dx0 - HORIZONTAL_OFFSET / zoom;
         var dy = dy0;
 
-        if (_imageRatio > _mandelRatio) {
-            var percentage = (_imageRatio / MANDEL_RATIO);
-            var difference = (RIGHT_EDGE - LEFT_EDGE) * percentage;
+        var leftEdge = LEFT_EDGE;
+        var rightEdge = RIGHT_EDGE;
+        var topEdge = TOP_EDGE;
+        var bottomEdge = BOTTOM_EDGE;
+
+        var ratio = 1;
+        var difference = 0;
+
+        if (this.imageRatio > MANDEL_RATIO) {
+            ratio = (this._imageRatio / MANDEL_RATIO);
+            difference = (RIGHT_EDGE - LEFT_EDGE) * ratio;
 
             //TODO: this shouldn't work
             leftEdge = -difference / (2 * (2.5 / 3.5));
             rightEdge = difference / (2 * (3.5 / 2.5));
 
-            topEdge = initTopEdge;
-            bottomEdge = initBottomEdge;
-        } else if (_imageRatio < _mandelRatio) {
-            var percentage = (_mandelRatio / _imageRatio);
-            var difference = (initBottomEdge - initTopEdge) * percentage;
+            topEdge = TOP_EDGE;
+            bottomEdge = BOTTOM_EDGE;
+        } else if (this._imageRatio < MANDEL_RATIO) {
+            ratio = (MANDEL_RATIO / this._imageRatio);
+            difference = (BOTTOM_EDGE - TOP_EDGE) * ratio;
+
             topEdge = -difference / 2;
             bottomEdge = difference / 2;
-            leftEdge = initLeftEdge;
-            rightEdge = initRightEdge;
+
+            leftEdge = LEFT_EDGE;
+            rightEdge = RIGHT_EDGE;
         }
 
-        pMax = rightEdge / zoom + dx;
-        pMin = leftEdge / zoom + dx;
-        qMax = bottomEdge / zoom + dy;
-        qMin = topEdge / zoom + dy;
+        var xMax = rightEdge / zoom + dx;
+        var xMin = leftEdge / zoom + dx;
+        var yMax = bottomEdge / zoom + dy;
+        var yMin = topEdge / zoom + dy;
 
-        xStep = (pMax - pMin) / _imageData.width;
-        yStep = (qMax - qMin) / _imageData.height;
+        var xStep = (xMax - xMin) / this._imageData.width;
+        var yStep = (yMax - yMin) / this._imageData.height;
 
         //An implementation of the Escape Time Algorithm
         //https://en.wikipedia.org/wiki/Mandelbrot_set#Escape_time_algorithm
-        for (var py = 0; py < _imageData.height; py++) {
-            for (var px = 0; px < _imageData.width; px++) {
+        for (var py = 0; py < this._imageData.height; py++) {
+            for (var px = 0; px < this._imageData.width; px++) {
                 //the canvas pixel data is a bit awkward to get at...
                 //see: https://www.w3.org/TR/2dcontext/#pixel-manipulation
-                var dataIndex = (py * _imageData.width + px) * 4;
 
                 //scale the pixel values to frame the bounds of the set
-                var x0 = pMin + xStep * px;
-                var y0 = qMin + yStep * py;
+                var x0 = xMin + xStep * px;
+                var y0 = yMin + yStep * py;
 
                 var x = 0.0;
                 var y = 0.0;
 
                 var iteration = 0;
-                while (x * x + y * y < 2 * 2 && iteration < _maxIterations) {
+                while (x * x + y * y < 2 * 2 && iteration < this._maxIterations) {
                     var tempX = x * x - y * y + x0;
                     y = 2 * x * y + y0;
                     x = tempX;
@@ -87,18 +108,13 @@ class Renderer {
 
                 //if we've maxed out our iterations, it's a close
                 //enough approximation, so color it black
-                var color = iteration === _maxIterations ? _palette._black : _palette.rainbow[iteration % _palette.length];
+                var color = iteration === this._maxIterations ? this._mandelbrotColor : this._palette.rainbow[iteration % this._palette.length];
 
-                _data[dataIndex] = color.r;
-                _data[dataIndex + 1] = color.g;
-                _data[dataIndex + 2] = color.b;
-                _data[dataIndex + 3] = 255;
+                this.plot(px, py, color);
             }
         }
         //draw it!
-        _context.putImageData(_imageData, 0, 0);
-        _writeStatus();
-        document.getElementById('get-png').href = _canvas.toDataURL('image/png');
+        this._context.putImageData(this._imageData, 0, 0);
     }
 
 }
